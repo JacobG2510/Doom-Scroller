f5cbc0aad05845dc5c50ebafa4ceff186560dea8
Heather Starr <starr.heather1@yahoo.com>
2023-10-02 21:23:50 -0700

    removed some code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink, } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+//import { ApolloClient,
+  //InMemoryCache,
+ // ApolloProvider,
+  //createHttpLink, } from '@apollo/client';
+//import { setContext } from '@apollo/client/link/context';
 import Home from './Pages/Home';
 import Search from './Pages/Search';
 import { useState } from 'react';
